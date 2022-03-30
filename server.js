e2cb16b50ad4f07a5588072045966acf796d9af3
prem <softwareengineerprem@gmail.com>
2022-03-30 20:08:35 +0530

    changed

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-    //Install express server
     const express = require('express');
     const path = require('path');
 
     const app = express();
 
-    // Serve only the static files form the dist directory
     app.use(express.static(__dirname + '/dist/<herokudemo>'));
 
     app.get('/*', function(req,res) {
@@ -12,5 +10,4 @@
     res.sendFile(path.join(__dirname+'/dist/<herokudemo>/index.html'));
     });
 
-    // Start the app by listening on the default Heroku port
     app.listen(process.env.PORT || 8080);
