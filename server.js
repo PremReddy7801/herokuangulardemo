ecf4eaa6c22700384f3a2702a63f514192131b23
prem <softwareengineerprem@gmail.com>
2022-03-30 23:30:28 +0530

    sdd

--- server.js
+++ server.js
@@ -10,4 +10,6 @@
     res.sendFile(path.join(__dirname+'/dist/<herokudemo>/index.html'));
     });
 
-    app.listen(8080);
+    app.listen(process.env.PORT || 3000, function(){
+        console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+      });
